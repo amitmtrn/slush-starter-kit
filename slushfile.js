f963faf497b022ce506faec7b4c3d2c094b93a6a
amitmtrn <amit@mtrn.co.il>
2015-07-07 21:43:51 +0300

    add inquirer

--- slushfile.js
+++ slushfile.js
@@ -1,6 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'); // the gulp running the tasks with gulp
+var inquirer = require('inquirer'); // ask the question
 
-gulp.task('default', function(done) {
-  gulp.src(__dirname + '/templates/app/**')
-  .pipe(gulp.dest('./'));
+gulp.task('default', function(done) { // build the package
+
+  // ask the questions
+  inquirer.prompt([
+    {type: 'input', name: 'name', message: 'What do you want to name your app?', default: getNameProposal()}
+  ],
+  function(answers) { // get the answers
+    console.log(answers);
+    gulp.src(__dirname + '/templates/app/**')
+    .pipe(gulp.dest('./'));
+  });
 });
+
+function getNameProposal() {
+  var path = require('path');
+  try {
+    return require(path.join(process.cwd(), 'package.json')).name;
+  } catch (e) {
+    return path.basename(process.cwd());
+  }
+}
